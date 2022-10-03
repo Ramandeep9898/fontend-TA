7646dfb3adbfe6075e6e5a52b09a27719fdb843a
Ramandeep <ramank6238299@gmail.com>
2022-10-03 22:25:47 +0530

    intial commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,21 @@
+export const LandingPage = () => {
+  return (
+    <>
+      <main className="landing-page-container">
+        <div className="main-heading">Imagine if snapchat had events.</div>
+        <div className="sub-heading">
+          Easily host and share events with your friends across any social
+          media.
+        </div>
+        <div className="image-container"></div>
+        <div className="create-my-event-btn">
+          <button>
+            {" "}
+            <span></span>
+            <span>create my event</span>{" "}
+          </button>
+        </div>
+      </main>
+    </>
+  );
+};
